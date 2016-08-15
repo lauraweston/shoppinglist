66ba8434210d28f3cfb8e418a2ed21a6fc1e384b
Laura Weston <laura.weston@outlook.com>
2016-08-15 22:14:47 +0100

    Don't let user get to login page if they are already logged in

--- app/controllers/authorisation_controller.rb
+++ app/controllers/authorisation_controller.rb
@@ -1,4 +1,8 @@
 class AuthorisationController < ApplicationController
   def login
+    if session[:userinfo].present?
+      # Redirect to page that has the login here
+      redirect_to '/'
+    end
   end
 end
