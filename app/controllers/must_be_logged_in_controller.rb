baab0a8696797beb924b0bf0791e520a91ca3d64
Laura Weston <laura.weston@outlook.com>
2016-08-20 21:20:08 +0100

    Display user email on page when user is logged in

--- app/controllers/must_be_logged_in_controller.rb
+++ app/controllers/must_be_logged_in_controller.rb
@@ -1,7 +1,7 @@
 class MustBeLoggedInController < ApplicationController
   
   before_action :logged_in_using_omniauth?
-
+  
   private
 
   def logged_in_using_omniauth?
@@ -9,6 +9,7 @@ class MustBeLoggedInController < ApplicationController
       # Redirect to page that has the login here
       redirect_to :controller => 'authorisation', :action => 'login'
     end
+    @user_email = session[:userinfo]["info"]["email"]
   end
 
 end
