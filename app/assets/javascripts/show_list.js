1506ae825431bd6a199bbd05719d0f775e3e2704
Robin Weston <lcfcforever@hotmail.com>
2016-09-03 15:24:11 +0100

    Toggle edit mode for list items

--- app/assets/javascripts/show_list.js
+++ app/assets/javascripts/show_list.js
@@ -1,5 +1,12 @@
 $(function() {
   
+  $(".edit-mode-btn").on('click', function(e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+
+    $(".show-in-edit-mode").toggle();  
+  });
+
   $("input.list-item-checkbox").change(function(){
     $(this).parent().toggleClass("item-purchased");
   });
